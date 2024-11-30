023422515524e10b52974e1dcdc4cb52ac870873
Curtis Elsasser <curt.elsasser@yahoo.com>
2024-11-29 21:04:12 -0500

    - add ticksPerQuarter to our CSV - working on calculating density - both of the above need review and testing

--- Elsasser/assembler/src/types.ts
+++ Elsasser/assembler/src/types.ts
@@ -1,38 +1,47 @@
-import {MidiIoEvent} from "midi-file-io";
+import {
+    MidiIoEvent,
+    MidiIoEventSubtype
+} from "midi-file-io";
 
 export interface Composer {
-	name: string;
-	yearBorn: number;
-	yearDied?: number;
+    name: string;
+    yearBorn: number;
+    yearDied?: number;
 }
 
 export interface CsvFile {
-	data: {
-		composer: string,
-		yearBorn: number,
-		yearDied: number,
-		title: string,
-		folder: string,
-		xml_score: string,
-		midi_score: string,
-		midi_performance: string,
-		performance_annotations: string,
-		midi_score_annotations: string,
-		maestro_midi_performance: string,
-		maestro_audio_performance: string,
-		start: number,
-		end: number,
-		audio_performance: string,
-		csv_score: string,
-		csv_performance: string
-	}[];
-	header: string[];
-	path: string;
+    data: {
+        composer: string,
+        yearBorn: number,
+        yearDied: number,
+        title: string,
+        folder: string,
+        xml_score: string,
+        midi_score: string,
+        midi_performance: string,
+        performance_annotations: string,
+        midi_score_annotations: string,
+        maestro_midi_performance: string,
+        maestro_audio_performance: string,
+        start: number,
+        end: number,
+        audio_performance: string,
+        csv_score: string,
+        csv_performance: string
+    }[];
+    header: string[];
+    path: string;
 }
 
 export interface MidiIoEventAbs extends MidiIoEvent {
-	tickOffset: number;
-	tickLength: number;
+    density?: number;
+    tickOffset: number;
+    tickLength: number;
+}
+
+export const MidiIoEventSubtypeExt = {
+    ...MidiIoEventSubtype,
+    TicksPerQuarter: "ticksPerQuarter"
 }
 
 export type MidiIoTrackAbs = MidiIoEventAbs[];
