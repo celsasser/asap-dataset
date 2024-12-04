6441d669e7111d118271ecfadbf9589ad7f74596
Curtis Elsasser <curt.elsasser@yahoo.com>
2024-12-03 21:56:09 -0500

    analysis: - cleanup wrangle functions. - get song loader to work as we want it to

--- Elsasser/analysis/wrangle.R
+++ Elsasser/analysis/wrangle.R
@@ -1,4 +1,5 @@
 # We came, we saw and we wrangled.
+library(purrr)
 library(tidyverse)
 
 #' Get the root of the ASAP project
@@ -16,7 +17,7 @@ get_asap_root <- function() {
 #' do some wrangling and separate them into two separate dataframes and
 #' eliminate the redundancy.
 #' @param root the root of the ASAP project
-#' @return a named `list` with two dataframes: `perfs` and `scores`
+#' @return a named `list` with two `tibbles`: `perfs` and `scores`
 load_manifest <- function(root = get_asap_root()) {
   manifest_path <- file.path(root, "metadata.csv")
   # we are adding an ID so that we can join performances and scores to their manifest.
@@ -24,7 +25,7 @@ load_manifest <- function(root = get_asap_root()) {
   tbl_perfs <- tbl_manifest |>
     mutate(id = row_number()) |>
     rename(path = csv_performance) |>
-    select(id, composer, yearBorn, yearDied, title, path)
+    select(id, composer, yearBorn, yearDied, title, performer, path)
   tbl_scores <- tbl_manifest |>
     distinct(composer, yearBorn, yearDied, title, csv_score) |>
     mutate(id = row_number()) |>
@@ -39,28 +40,45 @@ load_manifest <- function(root = get_asap_root()) {
 #' At the time of writing this I'm not sure whether the whole dataset will fit.
 #' And even if it does, it may be too slow or too much. So we shall let the user
 #' decide what to load.
-#' @param df all or part of the `perfs` or `scores` dataframe
-#' @return a `tibble`. We have joined it with the `df` so that it carries: `id',
-#'   `composer`, `yearBorn`, `yearDied`, `title`, and the music variables.
-#'  `df` row.
+#' Note: you may join the results of this function together using `bind_rows`.
+#'   Just plop the result of the function into `bind_rows` and you're golden.
+#'
+#' @param df all or part of the `perfs` or `scores` dataframe.
+#' @return one song `tibble` per row in `df`. We have joined each row with `df`
+#'  so that they carry the `id', `composer`, `yearBorn`, `yearDied`, `title` and
+#'  `performer` (if a performance) in addition to the music variables.
 load_music <- function(df) {
-  map(df, function(row) {
-    path = file.path(get_asap_root(), row$path)
-    read_csv(path, show_col_types = FALSE) |>
-      mutate(id = row$id)
-  }) |>
-    inner_join(df, by = "id") |>
-    select(-path)
+  pmap(df, function(id, path, ...) {
+    path = file.path(get_asap_root(), path)
+    tbl_song <- read_csv(path, show_col_types = FALSE)
+    # first, we are going to dig up the ticksPerQuarter from within the song.
+    # I didn't include it as a variable, cuz it's not really a music variable.
+    # A bigger reason I didn't include it as a variable is size. The catalog is
+    # getting chunky. But it's in there as a row. Let's get him 'cuz it's
+    # important metadata if one is going to do music math with ticks.
+    ticksPerQuarter <- tbl_song |>
+      filter(type == "ticksPerQuarter") |>
+      select(valueRaw) |>
+      mutate(valueRaw = as.integer(valueRaw))
+    # now we can load the music
+    tbl_song |>
+      filter(type == "note") |>
+      mutate(
+        id = id,
+        ticksPerQuarter = ticksPerQuarter$valueRaw
+      ) |>
+      inner_join(df, by = "id") |>
+      select(-path)
+  })
 }
 
 #' Give us the `perfs` or `scores` dataframe. Or give us part of it.
 #' We will do the filtering and return the dataframes you are looking for...
-#' fingers crossed.
-#' @param df the `perfs` or `scores` dataframe
-#' @param composer the composer to filter by
-#' @return a `tibble`. We have joined it with the `df` so that it carries: `id',
-#'   `composer`, `yearBorn`, `yearDied`, `title`, and the music variables.
-#'  `df` row.
+#' fingers crossed :).
+#'
+#' @param df the `perfs` or `scores` dataframe.
+#' @param composer the composer to filter by.
+#' @return see `load_music` for return information.
 load_music_by_composer <- function(df, composer) {
   df |>
     filter(composer == composer) |>
@@ -69,14 +87,26 @@ load_music_by_composer <- function(df, composer) {
 
 #' Give us the `perfs` or `scores` dataframe. Or give us part of it.
 #' We will do the filtering and return the dataframes you are looking for...
-#' hopefully.
-#' @param df the `perfs` or `scores` dataframe
-#' @param title the title of the song to filter by
-#' @return a `tibble`. We have joined it with the `df` so that it carries: `id',
-#'   `composer`, `yearBorn`, `yearDied`, `title`, and the music variables.
+#' hopefully :(.
+#'
+#' @param df the `perfs` or `scores` dataframe.
+#' @param title the title of the song to filter by.
+#' @return see `load_music` for return information.
 load_music_by_title <- function(df, title) {
   df |>
     filter(title == title) |>
+    load_music()
+}
+
+#' Give us the whole `perfs` dataframe and cross your fingers, or give us part
+#' of it. We will do the filtering and return the dataframes you are looking for.
+#'
+#' @param df the `perfs` dataframe.
+#' @param performer the performer to filter by.
+#' @return see `load_music` for return information.
+load_music_by_performer <- function(df, performer) {
+  df |>
+    filter(performer == performer) |>
     load_music()
 }
 
